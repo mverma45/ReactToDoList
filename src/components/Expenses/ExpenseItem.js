5013c62e79be74e0f6e1ea66c86677b03dc7c2c2
Manny Verma <mannyverma11@gmail.com>
2022-04-22 17:25:00 -0600

    adding more work

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,7 +5,8 @@ import Card from "../UI/Card"
 import "./ExpenseItem.css"
 
 const ExpenseItem = props => {
-  const [title, setTitle] = useState(props.title)
+  const [title, setTitle] = useState(props.title) // this props.title is only considered when the component function is being executed for the first time.
+  console.log("ExpenseItem created by React.")
   //useState needs a default state value you can't use it like useState, useState gives us access to the special variable, it doesn't just return that it also returns a function that we can then call to assign a new value to that variable.  So we won't be assigning values with an equal sign we will be assigning values by calling a function that's how state variable works.  State will return an array. The first value is the variable it self, the second is the updating function.  We can use the modern JS feature called array destructuring, the first element is pointer at that managed value 'useState(props.title)' the second is a function we can call later to set a new title.  We can pick any names, the convention is to use something that describes the value and then set plus the name of the value that updates the value but it can be what ever we want.  The useState 'the hook' always returns an array with two elements that's always the case.  With that we can remove the title variable. Also we can remove the value assignment in clickHandler
 
   // let title = props.title
