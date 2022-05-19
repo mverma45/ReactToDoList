fdc77cbc21075225871c7758de9da2c198d14d0b
Manny Verma <mannyverma11@gmail.com>
2022-05-18 19:54:11 -0600

    adding more work

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -103,6 +103,9 @@ const ExpenseForm = props => {
         </div>
       </div>
       <div className="new_expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
