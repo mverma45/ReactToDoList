4109fa7129107319e53b60e3a321dcc14544fe31
Manny Verma <mannyverma11@gmail.com>
2022-05-19 20:00:16 -0600

    adding work

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import ExpensesFilter from "./ExpensesFilter"
 import Card from "../UI/Card"
 import ExpensesList from "./ExpensesList"
+import ExpensesChart from "./ExpensesChart"
 import "./Expenses.css"
 
 function Expenses(props) {
@@ -24,6 +25,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
